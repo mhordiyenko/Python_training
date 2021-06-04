6aa001f95286d565f986a2153250ddd61029a823
Markiyan Hordiyenko <markhordi@gmail.com>
2021-06-04 15:36:54 +0300

    Update create_topic_and_subscription.py
    updated

--- PubSub/create_topic_and_subscription.py
+++ PubSub/create_topic_and_subscription.py
@@ -7,20 +7,13 @@ import google.api_core.exceptions
 from google.cloud import pubsub_v1
 
 
-class PubSubCreator:
+class PubSubTopicCreator:
 
-    def create(self):
-        pass
+    def __init__(self, publisher):
+        self.publisher = publisher
 
-class PubSubTopicCreator(PubSubCreator):
-
-    def __init__(self, publisher_obj, project_id_obj, topic_name_obj):
-        self.publisher = publisher_obj
-        self.project_id = project_id_obj
-        self.topic_name = topic_name_obj
-
-    def create(self):
-        topic_path = publisher.topic_path(project=self.project_id, topic=self.topic_name)
+    def create_new_topic(self, project_id, topic_name):
+        topic_path = self.publisher.topic_path(project_id, topic_name)
         try:
             self.publisher.get_topic(topic=topic_path)
             return print(f"The topic '{topic_path}' already exists.")
@@ -30,24 +23,20 @@ class PubSubTopicCreator(PubSubCreator):
             return print(f"The topic '{result.name}' is crated.")
 
 
-class PubSubSubscriptionCreator(PubSubCreator):
+class PubSubSubscriptionCreator:
 
-    def __init__(self, publisher_obj, subscriber_obj, project_id_obj, topic_name_obj, subscription_name_obj):
-        self.publisher = publisher_obj
-        self.subscriber = subscriber_obj
-        self.project_id = project_id_obj
-        self.topic_name = topic_name_obj
-        self.subscription_name = subscription_name_obj
+    def __init__(self, publisher, subscriber):
+        self.publisher = publisher
+        self.subscriber = subscriber
 
-    def create(self):
-        subscription_path = self.subscriber.subscription_path(project=self.project_id,
-                                                              subscription=self.subscription_name)
+    def create_new_subscription(self, project_id, topic_name, subscription_name):
+        subscription_path = self.subscriber.subscription_path(project_id, subscription_name)
         try:
             self.subscriber.get_subscription(subscription=subscription_path)
             return print(f"The subscription '{subscription_path}' already exists.")
         except google.api_core.exceptions.NotFound:
             print('Create new subscription -> in progress')
-            topic_path = self.publisher.topic_path(project=self.project_id, topic=self.topic_name)
+            topic_path = self.publisher.topic_path(project=project_id, topic=topic_name)
             result = self.subscriber.create_subscription(name=subscription_path, topic=topic_path)
             return print(f"The subscription '{result.name}' is crated.")
 
@@ -57,32 +46,32 @@ def parse_arguments():
     parser.add_argument('-pid', '--project_id', default='', help='Project ID. example: my-new-project')
     parser.add_argument('-t', '--topic', default='', help='Topic name. example: My_Topic')
     parser.add_argument('-sn', '--subscription_name', default='', help='Subscription name. example: my-sub')
+    return parser
+
+
+def check_input_arguments(parser):
     parameter = parser.parse_args()
-    if (not parameter.project_id) or (parameter.project_id == ''):
+    if not parameter.project_id:
         print('\nPlease input -pid, -t, -sn parameters to create topic and subscription or -pid, -t to create just '
               'topic.')
         exit(1)
-    if (not parameter.topic) or parameter.topic == '':
+    if not parameter.topic:
         print('\nPlease input -pid, -t, -sn parameters to create topic and subscription or -pid, -t to create just '
               'topic.')
         exit(1)
-    if (not parameter.subscription_name) or parameter.subscription_name == '':
-        subscription_name_inserted_obj = False
-    else:
-        subscription_name_inserted_obj = True
-    return parameter.project_id, parameter.topic, parameter.subscription_name, subscription_name_inserted_obj
+    return parameter.project_id, parameter.topic, parameter.subscription_name
 
 
 if __name__ == '__main__':
-    project_id, topic_name, subscription_name, subscription_name_inserted = parse_arguments()
-    publisher = pubsub_v1.PublisherClient()
-    subscriber = pubsub_v1.SubscriberClient()
+    project_id, topic_name, subscription_name = check_input_arguments(parse_arguments())
+    pubsub_publisher = pubsub_v1.PublisherClient()
+    pubsub_subscriber = pubsub_v1.SubscriberClient()
 
-    topic_creator = PubSubTopicCreator(publisher_obj=publisher, project_id_obj=project_id,
-                                       topic_name_obj=topic_name)
-    topic_creator.create()
-    if subscription_name_inserted:
-        subscription_creator = PubSubSubscriptionCreator(publisher_obj=publisher, subscriber_obj=subscriber,
-                                                         project_id_obj=project_id, topic_name_obj=topic_name,
-                                                         subscription_name_obj=subscription_name)
-        subscription_creator.create()
+    topic_creator = PubSubTopicCreator(pubsub_publisher)
+    topic_creator.create_new_topic(project_id, topic_name)
+    if subscription_name:
+        subscription_creator = PubSubSubscriptionCreator(pubsub_publisher, pubsub_subscriber)
+        subscription_creator.create_new_subscription(project_id, topic_name, subscription_name)
+
+
+# projects/maximal-cabinet-310908/topics/moon
